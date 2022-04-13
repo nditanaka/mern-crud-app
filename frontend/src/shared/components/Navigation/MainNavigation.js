188afd3c64d9be5865acccbc01cc8d99979bf10b
nditanaka <tanaching@gmail.com>
2022-04-13 18:43:51 -0400

    add mobile and desktop navigation

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -6,22 +6,30 @@ import { Link } from 'react-router-dom'
 import './MainNavigation.css'
 import NavLinks from './NavLinks'
 import MainHeader from './MainHeader'
+import SideDrawer from './SideDrawer'
 
-const MainNavigation = props => {
-  return (
-      <MainHeader>
-          <button className='main-navigation__menu-btn'>
-              <span />
-              <span />
-              <span />
-          </button>
-          <h1 className='main-navigation__title'>
-              <Link to='/'>Your Places</Link>
-          </h1>
-          <nav className='main-navigation__header-nav'>
-              <NavLinks />
-          </nav>
-    </MainHeader>
+const MainNavigation = (props) => {
+    return (
+        <>
+            <SideDrawer>
+            <nav className='main-navigation__drawer-nav'>
+                <NavLinks />
+                </nav>
+            </SideDrawer>
+            <MainHeader>
+                <button className='main-navigation__menu-btn'>
+                    <span />
+                    <span />
+                    <span />
+                </button>
+                <h1 className='main-navigation__title'>
+                    <Link to='/'>Your Places</Link>
+                </h1>
+                <nav className='main-navigation__header-nav'>
+                    <NavLinks />
+                </nav>
+            </MainHeader>
+        </>
   )
 }
 
