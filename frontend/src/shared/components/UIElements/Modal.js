5b159b350bb421228e7c1ce3dc75cdc0050b7105
nditanaka <tanaching@gmail.com>
2022-04-18 14:19:24 -0400

    add Google maps to PlaceItem component

--- frontend/src/shared/components/UIElements/Modal.js
+++ frontend/src/shared/components/UIElements/Modal.js
@@ -11,7 +11,7 @@ export const ModalOverlay = (props) => {
 
     const content = (
         <div className={`modal ${props.className}`} style={props.style}>
-        <header className={`modal_header ${props.headerClass}`}>
+        <header className={`modal__header ${props.headerClass}`}>
             <h2>{props.header}</h2>
         </header>
         <form onSubmit={props.onSubmit ? props.onSubmit : event => event.preventDefault()}
