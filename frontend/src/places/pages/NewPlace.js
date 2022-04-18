d630c383e9690145fb722b177ff7d4e7cfed41a1
nditanaka <tanaching@gmail.com>
2022-04-18 15:19:47 -0400

    add input validation to Input component

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { VALIDATOR_REQUIRE} from '../../shared/util/validators'
 import Input from '../../shared/components/FormElements/Input';
 import './NewPlace.css'
 
@@ -9,7 +10,7 @@ const NewPlace = () => {
       element='input'
       type='text'
       label='Title'
-      validators={[]}
+      validators={[VALIDATOR_REQUIRE()]}
       errorText='Please enter a valid title.'
     />
   </form>
