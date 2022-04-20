14d1d5efb9604bdaf4cfbc8709c3bc121c0115bf
nditanaka <tanaching@gmail.com>
2022-04-19 20:04:34 -0400

    re-use state logic for 'add place form' as custom hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,26 +8,30 @@ import {
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
     <Router>
       <MainNavigation />
       <main>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/:userId/places">
-          <UserPlaces />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Route path="/places/:placeId">
+            <UpdatePlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
       </main>
     </Router>
   );
--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,40 +1,41 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-import PlaceList from '../components/PlaceList'
+import PlaceList from '../components/PlaceList';
 
 const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Reserve Bank of Zimbabwe Building',
-        description: 'One of the tallest skyscrapers in Zimbabwe',
-        imageUrl: 'https://www.zimbabweyp.com/img/zw/h/1610726039-60-reserve-bank-of-zimbabwe.jpg',
-        address: 'New Reserve Bank Tower, Harare, Zimbabwe',
-        location: {
-            lat: -17.824858,
-            lng: 31.053028
-        },
-        creator:'u1'
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
     },
-    {
-        id: 'p2',
-        title: 'ZB Life Towers',
-        description: 'A skyscraper with a striking architectural style in Harare',
-        imageUrl: 'https://www.worldatlas.com/r/w1200/upload/c9/32/8c/untitled-design-387.jpg',
-        address: 'Monomotapa Building, Samora Machel Avenue, Harare, Zimbabwe',
-        location: {
-            lat: -17.824858,
-            lng: 31.053028
-        },
-        creator:'u2'
-    }
-]
-const UserPlaces = () => {
-    const userId = useParams().userId
-    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
-  return (
-      <PlaceList items={loadedPlaces}/>
-  )
-}
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u2'
+  }
+];
 
-export default UserPlaces
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
